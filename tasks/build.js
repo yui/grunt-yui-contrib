0d769b486bd9cf7424b62cf32f1af04a9c89dcd1
Dav Glass <davglass@gmail.com>
2013-04-15 14:31:46 -0500

    Skip grunt build if env var is present

--- tasks/build.js
+++ tasks/build.js
@@ -5,7 +5,13 @@ http://yuilibrary.com/license/
 */
 
 module.exports = function(grunt) {
-    grunt.registerTask('build', 'Building YUI', ['yogi-build', 'npm']);
+    if ('GRUNT_SKIP_BUILD' in process.env) {
+        grunt.registerTask('build', 'Building YUI', function() {
+            grunt.log.ok('Found GRUNT_SKIP_BUILD in environment, skipping build.');
+        });
+    } else {
+        grunt.registerTask('build', 'Building YUI', ['yogi-build', 'npm']);
+    }
     grunt.registerTask('build-test', 'Building and testing YUI', ['yogi-build', 'npm', 'test']);
     grunt.registerTask('all', 'Building and testing YUI', ['build-test']);
 };
