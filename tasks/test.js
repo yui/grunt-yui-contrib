0590d42b2e960fb452a5b1801fe7f5af737f18f6
Derek Gathright <drgath@gmail.com>
2013-04-30 22:48:04 -0700

    Added GRUNT_SKIP_TESTS to skip unit testing since it takes a while, which is no fun while debugging.

--- tasks/test.js
+++ tasks/test.js
@@ -10,50 +10,54 @@ var exec = require('child_process').spawn,
 
 module.exports = function(grunt) {
     var CLI = false;
-    
+
     grunt.registerTask('test-cli', 'Testing YUI via the CLI', function() {
         CLI = true;
         grunt.task.run('test');
-
     });
 
-    grunt.registerTask('test', 'Testing YUI', function() {
-        grunt.log.ok('Testing all modules with yogi');
-        
-        var done = this.async(),
-            yogi = path.join(process.cwd(), 'node_modules/yogi/bin/yogi.js'),
-            args = [
-                yogi,
-                'test'
-            ], child;
-
-        if (CLI) {
-            CLI = false;
-            args.push('--cli');
-        }
-
-        if ('TEST_RESULTS_DIR' in process.env) {
-            grunt.log.ok('Saving JUnitXML file to: ' + process.env.TEST_RESULTS_DIR);
-            args.push('--junit');
-            args.push('--outfile');
-            args.push(path.join(process.env.TEST_RESULTS_DIR, 'junit.xml'));
-        }
-
-        child = exec(process.execPath, args, {
-            cwd: path.join(process.cwd(), 'src'),
-            stdio: 'inherit',
-            env: process.env
+    if ('GRUNT_SKIP_TESTS' in process.env) {
+        grunt.registerTask('test', 'Testing YUI', function() {
+            grunt.log.ok('Found GRUNT_SKIP_TESTS in environment, skipping testing.');
         });
+    }
+    else {
+        grunt.registerTask('test', 'Testing YUI', function() {
+            grunt.log.ok('Testing all modules with yogi');
 
-        child.on('exit', function(code) {
-            if (code) {
-                grunt.fail.fatal('yogi test exited with code: ' + code);
+            var done = this.async(),
+                yogi = path.join(process.cwd(), 'node_modules/yogi/bin/yogi.js'),
+                args = [
+                    yogi,
+                    'test'
+                ], child;
+
+            if (CLI) {
+                CLI = false;
+                args.push('--cli');
             }
-            done();
+
+            if ('TEST_RESULTS_DIR' in process.env) {
+                grunt.log.ok('Saving JUnitXML file to: ' + process.env.TEST_RESULTS_DIR);
+                args.push('--junit');
+                args.push('--outfile');
+                args.push(path.join(process.env.TEST_RESULTS_DIR, 'junit.xml'));
+            }
+
+            child = exec(process.execPath, args, {
+                cwd: path.join(process.cwd(), 'src'),
+                stdio: 'inherit',
+                env: process.env
+            });
+
+            child.on('exit', function(code) {
+                if (code) {
+                    grunt.fail.fatal('yogi test exited with code: ' + code);
+                }
+                done();
+            });
         });
-
-    });
-
+    }
 };
 
 
