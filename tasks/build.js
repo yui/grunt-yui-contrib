be0bf016af88959efd8f77ffb11da1e1a330c093
Ezequiel Rodriguez <ezequiel@yahoo-inc.com>
2014-03-07 16:31:10 -0800

    Minimize the line length.

--- tasks/build.js
+++ tasks/build.js
@@ -8,7 +8,10 @@ module.exports = function(grunt) {
     // The `artifacts` directory will usually only ever in YUI's CI system.
     // If you're in CI, `build-npm` exists, and this task was called in
     // response to the `postinstall` hook; skip the build.
-    if ((process.env.npm_lifecycle_event === "postinstall" && grunt.file.exists('artifacts') && grunt.file.exists('build-npm')) || 'GRUNT_SKIP_BUILD' in process.env) {
+    var isPostinstall = process.env.npm_lifecycle_event === "postinstall",
+        isCI = grunt.file.exists('artifacts') && grunt.file.exists('build-npm');
+
+    if ('GRUNT_SKIP_BUILD' in process.env || (isPostinstall && isCI)) {
         grunt.registerTask('build', 'Building YUI', function() {
             grunt.log.ok('Found GRUNT_SKIP_BUILD in environment, skipping build.');
         });
